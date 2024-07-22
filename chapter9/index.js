669690160889880d058df8780a3a6400df8b62e4
Boitumelo <boitumelolefophane@gmail.com>
2024-07-22 13:05:36 +0200

    chapter 9 commits

---
+++ chapter9/index.js
@@ -0,0 +1,38 @@
+" use strict";
+
+/****************HISTORY MODEL************** */
+
+// console.dir(window);
+// console.dir(window.history.length);
+
+// console.dir(history);
+
+/****************NAVIGATOR OBJECT************** */
+
+// console.dir(window.navigator);
+
+// console.dir(navigator);
+
+/****************LOCATION OBJECT************** */
+// console.dir(location);
+
+/****************THE DOM************** */
+console.dir(document);
+
+const e1 = document.querySelector("h1");
+console.dir(e1);
+
+const alle = document.querySelectorAll(".output");
+console.log(alle);
+
+/****************LOCATION OBJECT************** */
+
+/****************LOCATION OBJECT************** */
+
+/****************LOCATION OBJECT************** */
+
+/****************LOCATION OBJECT************** */
+
+/****************LOCATION OBJECT************** */
+
+/****************LOCATION OBJECT************** */
