c3075cc364145495c1aa3e1dedc46ca3c483739f
Boitumelo <boitumelolefophane@gmail.com>
2024-07-29 08:21:25 +0200

    unfinished work

---
+++ chapter11/chapter11exercises/exercise4.js
@@ -0,0 +1,9 @@
+let act = document.querySelector(".action");
+
+let out = document.querySelector("p");
+
+out.textContent = "Hello Out There";
+
+act.style.height = "600px";
+
+act.style.width = "600px";
