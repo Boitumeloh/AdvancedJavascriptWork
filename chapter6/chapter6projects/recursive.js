69f3ad7cc8490798bf88418265a2fd9de24daaf6
Boitumelo <boitumelolefophane@gmail.com>
2024-08-02 12:38:08 +0200

    Exercises

---
+++ chapter6/chapter6projects/recursive.js
@@ -0,0 +1,17 @@
+function recursive(num) {
+  console.log(num);
+  if (num < 10) {
+    return  recursive(num+1);
+  }
+  return;
+}
+console.log(recursive(0));
+
+// const main = function counter(i) {
+//   console.log(i);
+//   if (i < 10) {
+//     return counter(i + 1);
+//   }
+//   return;
+// };
+// main(0);
