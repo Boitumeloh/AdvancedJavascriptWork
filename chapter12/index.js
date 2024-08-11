23cc5f1d976637b130d126f40c09f30e1d023710
Boitumelo <boitumelolefophane@gmail.com>
2024-08-11 11:52:32 +0200

    classwork

--- chapter12/index.js
+++ chapter12/index.js
@@ -17,26 +17,36 @@
 // }
 
 //Exercise 12.2
-let div = document.querySelector(".output");
-let input = document.querySelector("input");
-let btn = document.querySelector("button");
-let emailExp = /([A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z0-9]+)\w+/;
+// let div = document.querySelector(".output");
+// let input = document.querySelector("input");
+// let btn = document.querySelector("button");
+// let emailExp = /([A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z0-9]+)\w+/;
 
-btn.addEventListener("click", btnFun);
+// btn.addEventListener("click", btnFun);
 
-function btnFun(){
-    let cont = input.value;
-    let result = emailExp.test(cont);
-    let response = "";
+// function btnFun(){
+//     let cont = input.value;
+//     let result = emailExp.test(cont);
+//     let response = "";
 
-    if (!result){
-        response = "Invalid Email";
-        div.style.color = "red";
-    }else{
-        response = "Valid Email";
-        div.style.color = "green";
+//     if (!result){
+//         response = "Invalid Email";
+//         div.style.color = "red";
+//     }else{
+//         response = "Valid Email";
+//         div.style.color = "green";
+//     }
+//     input.value = "";
+//     div.textContent = response;
+// }
+
+//Exercise 12.3
+function create(){
+    let lastOne= "";
+    for(let i=0; i<arguments.length;i++){
+        lastOne = arguments[i];
     }
-    input.value = "";
-    div.textContent = response;
+    return lastOne;
 }
 
+console.log(create(29,24,43));
