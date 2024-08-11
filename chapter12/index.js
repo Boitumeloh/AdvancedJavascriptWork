dc721d49ede2a32fc90a01733f3bc9da29333349
Boitumelo <boitumelolefophane@gmail.com>
2024-08-11 11:20:06 +0200

    Projects

--- chapter12/index.js
+++ chapter12/index.js
@@ -1,11 +1,21 @@
 //Exercise 12.1
-let div = document.querySelector(".output");
-let input = document.querySelector(".sText");
-let input2 = document.querySelector(".rText");
+let div = document.querySelector("#output");
+let input = document.querySelector("#sText");
+let input2 = document.querySelector("#rText");
 let btn = document.querySelector("button");
 
-btn.addEventListener("click", function(){})
+btn.addEventListener("click", lookUp);
 
-function lookUp(){
-    s = div.replace()
+function lookUp() {
+  let s = div.textContent;
+  let rt = input2.value;
+  let reg = new RegExp(input.value, "gi");
+  if (s.match(reg)) {
+    let newValue = s.replace(reg, rt);
+    div.textContent = newValue;
+  }
 }
+
+
+//Exercise 12.2
+
