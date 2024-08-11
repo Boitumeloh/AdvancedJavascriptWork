44545a16efedb9cc930f0683eda97d7bf8aa13af
Boitumelo <boitumelolefophane@gmail.com>
2024-08-11 12:49:18 +0200

    classwork

--- chapter12/index.js
+++ chapter12/index.js
@@ -66,20 +66,76 @@
 // }
 
 //Exercise 12.5
-function checker(val){
-    try{
-        if (isNaN(val)){
-            throw "is not a number";
-        }else{
-            console.log("Got a number");
-            
-        }
-    }catch (e) {
-        console.error(e);
-    }finally{
-        console.log("Done "+ val);
-    }
-}
+// function checker(val){
+//     try{
+//         if (isNaN(val)){
+//             throw "is not a number";
+//         }else{
+//             console.log("Got a number");
 
-checker("b");
-checker(20);
+//         }
+//     }catch (e) {
+//         console.error(e);
+//     }finally{
+//         console.log("Done "+ val);
+//     }
+// }
+
+// checker("b");
+// checker(20);
+
+//Exercise 12.7
+const userTask = document.querySelector(".main input");
+const addBtn = document.querySelector(".main button");
+const output = document.querySelector(".output");
+const tasks = JSON.parse(localStorage.getItem("tasklist")) || [];
+addBtn.addEventListener("click", createListItem);
+if (tasks.length > 0) {
+  tasks.forEach((task) => {
+    genItem(task.val, task.checked);
+  });
+}
+function saveTasks() {
+  localStorage.setItem("tasklist", JSON.stringify(tasks));
+}
+function buildTasks() {
+  tasks.length = 0;
+  const curList = output.querySelectorAll("li");
+  curList.forEach((el) => {
+    const tempTask = {
+      val: el.textContent,
+      checked: false,
+    };
+    if (el.classList.contains("ready")) {
+      tempTask.checked = true;
+    }
+    tasks.push(tempTask);
+  });
+  saveTasks();
+}
+function genItem(val, complete) {
+  const li = document.createElement("li");
+  const temp = document.createTextNode(val);
+  li.appendChild(temp);
+  output.append(li);
+  userTask.value = "";
+  if (complete) {
+    li.classList.add("ready");
+  }
+  li.addEventListener("click", (e) => {
+    li.classList.toggle("ready");
+    buildTasks();
+  });
+  return val;
+}
+function createListItem() {
+  const val = userTask.value;
+  if (val.length > 0) {
+    const myObj = {
+      val: genItem(val, false),
+      checked: false,
+    };
+    tasks.push(myObj);
+    saveTasks();
+  }
+}
