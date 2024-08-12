bc6b016d76881ea9fb6330f4b80e0655a84ae6c0
Boitumelo <boitumelolefophane@gmail.com>
2024-08-12 08:30:00 +0200

    classwork

---
+++ chapter13/index.js
@@ -0,0 +1,13 @@
+//Exercise 13.1
+// function greet(fullName){
+//     console.log(`Welcome, ${fullName[0]} ${fullName[1]}`)
+// }
+
+// function sec_greet(UsersFullName, callback){
+//    let fullName =  UsersFullName.split(" ");
+//    callback(fullName);
+// }
+
+// sec_greet("Boitumelo Skevis", greet);
+
+//Exercise 13.2
